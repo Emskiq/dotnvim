d70e26d3065d04fd3949a1439a70a8fdca76b8e2
Emil Tsanev <emil.tsanev@amusnet.com>
2024-01-22 11:24:26 +0200

    add keymap for Gitsign column

--- lua/emilski/remap.lua
+++ lua/emilski/remap.lua
@@ -24,3 +24,6 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 
 -- Clear last search highlights
 vim.keymap.set("n", "<Esc>", "<cmd>noh<CR>")
+
+-- Set GitSign column Transperant (when changing the background to light/dark)
+vim.keymap.set("n", "<leader>gs", "<cmd>highlight SignColumn guibg=NONE<CR>")
