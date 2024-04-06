ae3a46e5aa1645a3cd9b8ae7751759d5ea016407
Emil Tsanev <emil.tsanev@amusnet.com>
2024-04-06 13:16:35 +0300

    add git hunk navigation

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -2,3 +2,32 @@ require('gitsigns').setup()
 
 --- Set GitSign column Transperant (when changing the background to light/dark)
 vim.keymap.set("n", "<leader>gs", "<cmd>highlight SignColumn guibg=NONE<CR>")
+
+require('gitsigns').setup{
+    on_attach = function(bufnr)
+        local gitsigns = require('gitsigns')
+
+        local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map('n', ']c', function()
+            if vim.wo.diff then
+                vim.cmd.normal({']c', bang = true})
+            else
+                gitsigns.nav_hunk('next')
+            end
+        end)
+
+        map('n', '[c', function()
+            if vim.wo.diff then
+                vim.cmd.normal({'[c', bang = true})
+            else
+                gitsigns.nav_hunk('prev')
+            end
+        end)
+    end
+}
